86d55c20e90d4525a15081bfb995189ccafa50f8
Kadeka SENG <kadekaseng@gmail.com>
2016-03-22 15:45:59 +0700

    Menu category link (check router)

--- client/Application/frontend/Controller/header.js
+++ client/Application/frontend/Controller/header.js
@@ -1,12 +1,12 @@
-Template.header.events({
-	'click #listCate':function(e){
-		e.preventDefault();
-		var id = this._id;
-		Session.set("categoriesId",id);
-		Router.go("beauty");
-		//alert(id);
-	}
-});
+// Template.header.events({
+// 	'click #listCate':function(e){
+// 		e.preventDefault();
+// 		var id = this._id;
+// 		Session.set("categoriesId",id);
+// 		Router.go("beauty");
+// 		//alert(id);
+// 	}
+// });
 Template.header.rendered = function(){
 	new UISearch( document.getElementById( 'sb-search' ) );
 };
